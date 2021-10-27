4858f123b83fde4fa1a4ce86807b72f2d15a042d
Joan Salvador <joansm116@gmail.com>
2021-10-27 15:05:22 +0200

    Create Spring Boot project

---
+++ everis-dar-mytasks-ms/src/test/java/com/nttdata/everisdarmytasksms/EverisDarMytasksMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.everisdarmytasksms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverisDarMytasksMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
