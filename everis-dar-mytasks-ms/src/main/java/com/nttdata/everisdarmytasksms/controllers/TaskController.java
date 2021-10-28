a47cd597aeb6734be797df70bfd711cffbc3011a
Joan Salvador <joansm116@gmail.com>
2021-10-28 15:38:57 +0200

    Get task by id

--- everis-dar-mytasks-ms/src/main/java/com/nttdata/everisdarmytasksms/controllers/TaskController.java
+++ everis-dar-mytasks-ms/src/main/java/com/nttdata/everisdarmytasksms/controllers/TaskController.java
@@ -5,9 +5,12 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.nttdata.everisdarmytasksms.repositories.TaskRepository;
 
@@ -28,6 +31,16 @@ public class TaskController {
 		resp.setMsg("Task is succesfully added");
 		resp.setId(task.getId());
 		return new ResponseEntity<AddResponse>(resp, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/tasks/{id}")
+	public Task getTaskById(@PathVariable(value="id") int id) {
+		try {
+			Task task = repository.findById(id).get();
+			return task;
+		} catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found");
+		}
 	}
 
 }
